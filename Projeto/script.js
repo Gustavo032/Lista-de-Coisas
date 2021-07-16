900116b305d0a78b87edc474bf97fd1387950387
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-07-16 13:20:57 -0300

    terminando o projeto

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,32 +1,45 @@
 let input = document.getElementById("inputText")
 let button = document.getElementById("btnAdd")
 let lista = document.getElementById("listaUl")
-let btnDel = document.getElementById("delete")
+let btnDel = document.getElementsByClassName("delete")
+let itens = document.getElementsByTagName("li")
+
 
 function createListItens() {
-    let li = document.createElement("li")
+    if (input.value == "") {
+        alert("Você deve preencher o campo antes de adicionar :D")
+    } else {
+        let li = document.createElement("li")
 
-    let xButton = document.createElement("button")
-    
-    xButton.innerHTML = "X"
-    xButton.className = "delete"
-    
-    li.appendChild(xButton)
+        let xButton = document.createElement("button")
+        xButton.innerHTML = "X"
+        xButton.className = "delete"
+        
+        li.appendChild(xButton)
 
-    let valueText = document.createTextNode(input.value)
-    
-    li.appendChild(valueText)
-     
-    lista.appendChild(li)    
-    
-    
-    input.value = ""
+        let valueText = document.createTextNode(input.value)
+        
+        li.appendChild(valueText)
+        
+        lista.appendChild(li)    
+        
+        
+        input.value = ""
+        
+        itensEvents()
+    }
 }
 
 button.addEventListener("click", createListItens)
 
-function deleteListItens() {
-    
+function deleteListItens(){
+    this.parentElement.remove();
 }
 
-btnDel.addEventListener("click", deleteListItens)
+function itensEvents() {
+    for (let i = 0; i < itens.length; i++ ) {
+        btnDel[i].addEventListener("click", deleteListItens)
+    }
+}
+
+itensEvents()
